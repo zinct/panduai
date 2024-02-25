9eadbfd0e242ecaaeda87aebed59d1b16919d01a
Indra Mahesa <indramahesa128@gmail.com>
2024-02-25 21:16:40 +0700

    setup: inital project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginView from '@/feature/auth/LoginView';
+
+const page = () => {
+    return (
+        <LoginView />
+    );
+};
+
+export default page;
---
+++ src/feature/auth/LoginView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginView = () => {
+    return (
+        <div className="bg-neutral-1">
+            <h2>Hello World</h2>
+        </div>
+    );
+};
+
+export default LoginView;
